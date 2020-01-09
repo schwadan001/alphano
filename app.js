8852d31dc4f41c3a0191eabe6ce084ef6887687d
schwadan001 <schwadan001@gmail.com>
2020-01-09 15:06:08 -0600

    integrate lozza

--- app.js
+++ app.js
@@ -2,25 +2,11 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const path = require("path");
-const Ai = require("./js/ai.js");
 const port = 8080;
 
 // HTML page routing
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/web/index.html"));
-})
-
-/* Host files for internal and external reference */
-app.get("/sitemap.xml", function (req, res) {
-  res.sendFile(path.join(__dirname + "/sitemap.xml"));
-})
-
-app.get("/google63001e0b6d2bf67b.html", function (req, res) {
-  res.sendFile(path.join(__dirname + "/google63001e0b6d2bf67b.html"));
-})
-
-app.get("/favicon.ico", function (req, res) {
-  res.sendFile(path.join(__dirname + "/favicon.ico"));
 })
 
 app.get("/web/index.css", function (req, res) {
@@ -39,20 +25,30 @@ app.get("/js/chessboard-1.0.0.min.js", function (req, res) {
   res.sendFile(path.join(__dirname + "/js/chessboard-1.0.0.min.js"));
 })
 
+app.get("/js/lozza.js", function (req, res) {
+  res.sendFile(path.join(__dirname + "/js/lozza.js"));
+})
+
+app.get("/js/lozuilib.js", function (req, res) {
+  res.sendFile(path.join(__dirname + "/js/lozuilib.js"));
+})
+
 app.get("/img/chesspieces/wikipedia/:img", function (req, res) {
   let img = req.params.img;
   res.sendFile(path.join(__dirname + "/img/chesspieces/wikipedia/" + img));
 })
 
-
-/* Handle GET requests for game actions */
-app.get("/api/ai/:fen", function (req, res) {
-  let fen = req.params.fen.split("|").join("/");
-  Ai.getAiMove(fen, 2, function(move) {
-    res.send({"move": move});
-  })
+app.get("/favicon.ico", function (req, res) {
+  res.sendFile(path.join(__dirname + "/favicon.ico"));
 })
 
+app.get("/sitemap.xml", function (req, res) {
+  res.sendFile(path.join(__dirname + "/sitemap.xml"));
+})
+
+app.get("/google63001e0b6d2bf67b.html", function (req, res) {
+  res.sendFile(path.join(__dirname + "/google63001e0b6d2bf67b.html"));
+})
 
 // return 404 status for invalid pages - will remove from Google indexing
 app.get('*', function (req, res) {
